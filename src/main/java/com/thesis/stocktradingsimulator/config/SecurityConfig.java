a3c0dfc6f933d2e408db14a49a16e9a3dfcf1960
elenaplesu <elena.plesu44@gmail.com>
2026-04-09 17:19:10 +0300

    testing with disabled csrf

--- src/main/java/com/thesis/stocktradingsimulator/config/SecurityConfig.java
+++ src/main/java/com/thesis/stocktradingsimulator/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -43,10 +44,11 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-                .csrf(csrf -> csrf
-                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                        .csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler())
-                )
+                //.csrf(csrf -> csrf
+                //        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                //        .csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler())
+                //)
+                .csrf(AbstractHttpConfigurer::disable)
                 .securityContext(context -> context
                         .securityContextRepository(securityContextRepository())
                 )
